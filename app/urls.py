1fa8e19060e897625a8c0ac888635f29d03d7019
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-04-27 17:47:14 -0400

    added static files urls

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('blog', views.blog, name="blog"),
     path('contacts', views.contacts, name="contacts"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
