4b7a263495b12a0bef2deb6abfd10093ba2316cc
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-04-27 07:35:28 -0400

    changed views definition

--- app/views.py
+++ app/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'app/home.html')
 
 def services(request):
-    return HttpResponse('Services')
+    return render(request, 'app/services.html')
 
 def store(request):
-    return HttpResponse('Store')
+    return render(request, 'app/store.html')
 
 def blog(request):
-    return HttpResponse('Blog')
+    return render(request, 'app/blog.html')
 
 def contacts(request):
-    return HttpResponse('Contacts')
+    return render(request, 'app/contacts.html')
