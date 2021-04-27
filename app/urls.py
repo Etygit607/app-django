1336b8590a45b195670ad695904cba970d882016
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-04-27 19:24:45 -0400

    moved code from app to services

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('services', views.services, name="services"),
     path('store', views.store, name="store"),
     path('blog', views.blog, name="blog"),
     path('contacts', views.contacts, name="contacts"),
