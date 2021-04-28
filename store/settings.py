c228d4c631b43ef636dbef5e23cb32c35b579374
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-04-28 17:08:24 -0400

    added email config

--- store/settings.py
+++ store/settings.py
@@ -130,3 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+#email config
+EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="example@gmail.com"
+EMAIL_HOST_PASSWORD=""
