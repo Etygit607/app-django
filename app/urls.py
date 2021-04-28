dea1dbfcd4bf6780a09051f47989562160e78d78
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-04-28 18:01:22 -0400

    moved urls code from app to shop

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('store', views.store, name="store"),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
