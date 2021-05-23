92dbad11cf4e28853317a6b9185c2a581ff0684e
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-05-23 09:09:06 -0400

    added shoping functions

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .cart import Cart
+from shop.models import Product
 
 # Create your views here.
+
+def add_product(request, product_id):
+    cart = Cart(request)
+    product = Product.objects.get(id = product_id)
+    cart.add(product = product)
+    return redirect('shop')
+
+def delete_product(request, product_id):
+    cart = Cart(request)
+    product = Product.objects.get(id = product_id)
+    cart.delete(product = product)
+    return redirect('shop')
+
+def delete_one_product(request, product_id):
+    cart = Cart(request)
+    product = Product.objects.get(id = product_id)
+    cart.delete_one(product = product)
+    return redirect('shop')
+
+def empty_cart(request):
+    cart = Cart(request)
+    cart.clean_cart()
+    return redirect('shop')
