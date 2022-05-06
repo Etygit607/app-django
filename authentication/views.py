2bf6b812c8366e753db8338249d64f2c4198cd6c
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2022-05-06 03:40:35 -0400

    adición - registro de usuario y cierre de sessión

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -10,4 +12,18 @@ class Register(View):
         return render(request, "register/register.html", {"form" : form})
     
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else :
+            for message in form.error_messages:
+                messages.error(request, form.error_messages)
+            
+            return render(request, "register/register.html", {"form": form})
+
+def sign_out(request):
+    logout(request)
+    return redirect('home')
