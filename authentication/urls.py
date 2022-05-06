506537e3b3028452cfcac97afd8a018c497a9351
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2022-05-06 03:54:13 -0400

    adición - ruta login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Register, sign_out
+from .views import Register, sign_in, sign_out
 
 
 urlpatterns = [
     path('', Register.as_view(), name="sign_up"),
+    path('login/', sign_in , name="sign_in"),
     path('logout/', sign_out , name="sign_out"),
 ]
