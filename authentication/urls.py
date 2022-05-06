70778d8dcdabbc07e421edb0fb1f6f1cc695c8d1
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2022-05-06 03:42:21 -0400

    adición - rutas de registro y cierre de sessión

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Register
+from .views import Register, sign_out
 
 
 urlpatterns = [
-    path('', Register.as_view(), name="Authentication"),
+    path('', Register.as_view(), name="sign_up"),
+    path('logout/', sign_out , name="sign_out"),
 ]
