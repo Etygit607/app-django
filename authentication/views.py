d60e853ad9e09df77194525e334562d3232e3b4b
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2022-05-06 04:15:55 -0400

    modificación - funcionalidad de la vista login

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
@@ -29,5 +29,20 @@ def sign_out(request):
     return redirect('home')
 
 def sign_in(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Usuario no válido")
+
+        else:
+            messages.error(request, "Información incorrecta")
+
     form = AuthenticationForm()
     return render(request, "login/login.html", {"form": form})
