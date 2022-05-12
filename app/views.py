d6b9afd81c3e974897da5da7384edef2e9b45885
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2022-05-12 18:03:02 -0400

    modificación - debug de vistas

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+from cart.cart import Cart
+
+
 # Create your views here.
 
 def home(request):
+    cart = Cart(request)
     return render(request, 'app/home.html')
-
-def store(request):
-    return render(request, 'app/store.html')
